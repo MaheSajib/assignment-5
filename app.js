9d999868318f3994e1162d7bee123963e31f40f6
MaheSajib <mahe.sajib@gmail.com>
2021-02-08 20:48:03 +0600

    adding last section

--- app.js
+++ app.js
@@ -14,20 +14,13 @@ const displayMeals = meals =>{
         mealDiv.className = 'mealList'
         mealDiv.innerHTML =`
         <img id="thumbPic" src="${meal.strMealThumb}">
-        <h2 onclick="mealDetail('${meal.strMealThumb}','${meal.strMeal}')">${meal.strMeal}</h2>
-    `
+        <h2 onclick="mealDetail('${meal.strIngredient1}','${meal.strIngredient2}','${meal.strIngredient3}','${meal.strIngredient4}','${meal.strIngredient5}','${meal.strIngredient6}','${meal.strIngredient7}','${meal.strIngredient8}','${meal.strIngredient9}','${meal.strIngredient10}')">${meal.strMeal}</h2>
+        `
         mealList.appendChild(mealDiv);
     });
 }
 
-const mealDetail = (img, name) =>{
-    
-    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=+${img}/${name}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayMealsDetails(data.meal))
-}
-const displayMealsDetails = ingredient => {
+const mealDetail = ( name,name2,name3,name4,name5,name6,name7,name8,name9,name10) =>{
     const detailsDiv = document.getElementById('meal-detail');
-    detailsDiv.innerText = ingredient;
+    detailsDiv.innerText = name + '  '+ name2+ '  '+ name3+ '  '+ name4+ '  '+ name5+ '  '+ name6+ '  '+ name7+ '  '+ name8+ '  '+ name9+ '  '+ name10;
 }
